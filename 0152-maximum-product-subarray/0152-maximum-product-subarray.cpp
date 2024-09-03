f83d8aafeebc20d197c6abb120be611a6fb9c140
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-09-03 11:01:45 +0530

    Time: 5 ms (59.08%), Space: 16.6 MB (73.66%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -7,7 +7,8 @@ public:
        
         int n=nums.size();
         for(int i=0;i<nums.size();i++){
-            
+            if(pre==0)pre=1;
+            if(suf==0)suf=1;
             pre*=nums[i];
             suf*=nums[n-i-1];
             int temp=pre;
