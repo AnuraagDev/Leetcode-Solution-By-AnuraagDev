2324e0e85c3f4454f74fb38a34d18bd3e7fd9d53
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-09-21 16:16:33 +0530

    Time: 156 ms (86.02%), Space: 156.7 MB (9.75%) - LeetHub

--- 2393-match-substring-after-replacement/2393-match-substring-after-replacement.cpp
+++ 2393-match-substring-after-replacement/2393-match-substring-after-replacement.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool isMatch(string& s,string &sub,vector<vector<int>>&mp){
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]!=sub[i]){
-                char oldi=sub[i],newi=s[i];
-                if(!mp[oldi][newi])return false;
-            }
-        }return 1;
-    }
     bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
         vector<vector<int>>mp(256,vector<int>(256,0));
         int n=s.size(),m=sub.size();
-        for(auto it:mappings){
-            char oldi=it[0],newi=it[1];
-            mp[oldi][newi]=1;
+        for(auto i:mappings){
+            char a=i[0],b=i[1];
+            mp[a][b]=1;
         }
-        for(auto i=0;(i+m)<=n;i++){
+        for(int i=0;i+m<=n;i++){
             string temp=s.substr(i,m);
-            if(isMatch(temp,sub,mp))return true;
+            if(solve(temp,sub,mp))return true;
+
         }return false;
+    }
+    bool solve(string &s,string &sub,vector<vector<int>>&mp){
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]!=sub[i]){
+                char a=sub[i],b=s[i];
+                if(!mp[a][b])return false;
+            }
+        }return true;
     }
 };
