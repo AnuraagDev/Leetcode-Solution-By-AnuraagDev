f80756a175d7ad470b7bd6468fb1c8b5ce249be1
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-09-21 20:13:11 +0530

    Time: 168 ms (84.32%), Space: 156.9 MB (9.75%) - LeetHub

--- 2393-match-substring-after-replacement/2393-match-substring-after-replacement.cpp
+++ 2393-match-substring-after-replacement/2393-match-substring-after-replacement.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
+        int n=s.size();
+        int m=sub.size();
         vector<vector<int>>mp(256,vector<int>(256,0));
-        int n=s.size(),m=sub.size();
         for(auto i:mappings){
-            char a=i[0],b=i[1];
+            char a=i[0];
+            char b=i[1];
             mp[a][b]=1;
         }
         for(int i=0;i+m<=n;i++){
             string temp=s.substr(i,m);
             if(solve(temp,sub,mp))return true;
-
         }return false;
     }
-    bool solve(string &s,string &sub,vector<vector<int>>&mp){
-        int n=s.size();
-        for(int i=0;i<n;i++){
+    bool solve(string &s, string &sub,vector<vector<int>>&mp){
+        for(int i=0;i<s.size();i++){
             if(s[i]!=sub[i]){
                 char a=sub[i],b=s[i];
-                if(!mp[a][b])return false;
+                if(mp[a][b]==0)return false;
             }
-        }return true;
+        }return 1;
     }
 };
