d900a03d38e6cb3fc776143d12c98b4886734587
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-10-08 10:07:25 +0530

    Time: 3 ms (37.48%), Space: 7.9 MB (98.69%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>triangle(numRows);
+        for(auto i=0;i<numRows;i++){
+            triangle[i].resize(i+1);
+            triangle[i][0]=1;
+            triangle[i][i]=1;
+        }
+        for(auto i=1;i<numRows;i++){
+            for(auto j=1;j<i;j++){
+              
+            triangle[i][j]=triangle[i-1][j-1]+triangle[i-1][j];                
+        }}
+        return triangle;
+    }
+};
