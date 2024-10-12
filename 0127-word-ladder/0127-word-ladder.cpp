1f99676c21a9c2b35687ed93e9789ce321f80f14
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-10-12 15:18:11 +0530

    Time: 62 ms (82.46%), Space: 19.5 MB (87.59%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -6,18 +6,19 @@ public:
         unordered_set<string>se(wordList.begin(),wordList.end());
         if(se.find(endWord)==se.end())return 0;
         q.push({beginWord,1});
+        se.erase(beginWord);
         while(!q.empty()){
             string word=q.front().first;
             int step=q.front().second;
             q.pop();
             if(word==endWord)return step;
-            se.erase(word);
             for(int i=0;i<word.length();i++){
                 char original=word[i];
                 for(char c='a';c<='z';c++){
                     word[i]=c;
                     if(se.find(word)!=se.end()){
                         q.push({word,step+1});
+                        se.erase(word);
                     }
 
                 }
