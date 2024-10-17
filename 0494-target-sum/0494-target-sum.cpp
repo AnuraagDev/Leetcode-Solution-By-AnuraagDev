b2c6af65db566d5a28bcf451cad30266074754ba
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-10-17 23:56:21 +0530

    Time: 17 ms (49.16%), Space: 30.4 MB (23.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0,nums,target);
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(abs(target)>sum)return 0;
+        vector<vector<int>>dp(nums.size(),vector<int>(3*sum+1,-1));
+        return solve(0,nums,target,sum,dp);
     }
-    int solve(int i,vector<int>&nums,int target){
-       if(i>=nums.size()){
-        if(target==0){
-            return 1;
+    int solve(int i,vector<int>&nums,int target,int of,vector<vector<int>>&dp){
+        if(i==nums.size()){
+            if(target==0)return 1;
+            return 0;
         }
-    return 0;}
-    int include=solve(i+1,nums,target-nums[i]);
-    int exclude=solve(i+1,nums,target+nums[i]);
-    return include+exclude;
+        if(dp[i][target+of]!=-1)return dp[i][target+of];
+        int include=solve(i+1,nums,target-nums[i],of,dp);
+        int exclude=solve(i+1,nums,target+nums[i],of,dp);
+        return dp[i][target+of]=include+exclude;
     }
 };
