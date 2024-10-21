b39a54acd69e13d8cc557667d48ba6c9cdff6ecd
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-10-21 22:32:27 +0530

    Time: 0 ms (100%), Space: 70.6 MB (20.45%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int curr=0;
+        for(auto i:nums){
+            curr+=i;
+            maxi=max(curr,maxi);
+            if(curr<0)curr=0;
+        }
+        return maxi;
+    }
+};
