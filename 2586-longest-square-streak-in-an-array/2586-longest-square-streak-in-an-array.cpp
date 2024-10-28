7d6e96a73b3dbef17769cc051e91ee5ea704d3c6
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-10-28 20:58:18 +0530

    Time: 181 ms (11.11%), Space: 137.1 MB (9.33%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,21 +1,33 @@
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("O3", "unroll-loops")
+#pragma GCC optimize ("-ffloat-store")
+
+auto init = []() { 
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    return 0;
+} ();
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
         unordered_set<long long> mp(nums.begin(), nums.end());
+        unordered_map<long long,int>m1;
         int maxStreak = 0;
-
+        //sort(nums.begin(),nums.end());
         for (auto i : nums) {
-            maxStreak = max(maxStreak, solve(i, mp));
+            maxStreak = max(maxStreak, solve(i, mp,m1));
         }
 
         return maxStreak == 1 ? -1 : maxStreak;
     }
 
 private:
-    int solve(long long i, unordered_set<long long>& mp) {
+    int solve(long long i, unordered_set<long long>& mp, unordered_map<long long,int>&m1) {
+        if(m1.find(i)!=m1.end())return m1[i];
         if (mp.find(i * i) != mp.end()) {
-            return 1 + solve(i * i, mp);
+            
+            return m1[i]=1 + solve(i * i, mp,m1);
         }
-        return 1; 
+        return m1[i] = 1; 
     }
 };
