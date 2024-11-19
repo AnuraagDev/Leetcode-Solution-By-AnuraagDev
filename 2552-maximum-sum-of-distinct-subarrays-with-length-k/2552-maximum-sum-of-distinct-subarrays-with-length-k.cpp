94855f06d39b3b2f5e0c52515013d3a3f6316cc8
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-11-19 00:51:28 -0800

    Time: 98 ms (88.33%), Space: 95.2 MB (57.32%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> freq; // To track frequency of elements
+        unordered_map<int, int> freq; 
         long long sum = 0, maxi = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            // Add current element to the window
             sum += nums[i];
             freq[nums[i]]++;
 
-            // Shrink the window if it exceeds size k
             if (i >= k) {
                 sum -= nums[i - k];
                 freq[nums[i - k]]--;
@@ -18,7 +16,6 @@ public:
                 }
             }
 
-            // Check if the current window is valid and update the maximum sum
             if (freq.size() == k) {
                 maxi = max(maxi, sum);
             }
