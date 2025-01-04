9eebf8126179f23d2467c7110b511d696ec655d4
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2025-01-04 20:16:20 +0530

    Time: 72 ms (93.87%), Space: 14.2 MB (94.79%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -5,12 +5,16 @@ public:
         int res = 0;
         for (char a = 'a'; a <= 'z'; ++a) {
             int l = 0, r = n - 1;
-            while (l < n && s[l] != a) ++l;
-            while (r >= 0 && s[r] != a) --r;
+            while (l < n && s[l] != a)
+                ++l; // check for the first occurence
+            while (r >= 0 && s[r] != a)
+                --r; // check for the last occurence
             if (l < r) {
                 vector<bool> seen(26, false);
-                for (int i = l+1;i<r;++i) {
-                    seen[s[i]-'a'] = true;
+                for (int i = l + 1; i < r; ++i) {
+                    seen[s[i] - 'a'] =
+                        true; // check for all different alpha available for
+                              // purpose of unique calculation
                 }
                 res += count(seen.begin(), seen.end(), true);
             }
