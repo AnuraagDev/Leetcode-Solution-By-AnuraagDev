3b567209cf4d15daf1d5ff726604001bb3d26637
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2025-01-06 18:51:17 +0530

    Time: 96 ms (97.6%), Space: 228.9 MB (92.47%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        vector<vector<pair<int, int>>> adjacencyList(n);
+    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int src, int dest, int target) {
+        vector<vector<pair<int, int>>> graph(n);
         for (int i = 0; i < edges.size(); i++) {
-            int nodeA = edges[i][0], nodeB = edges[i][1];
-            adjacencyList[nodeA].emplace_back(nodeB, i);
-            adjacencyList[nodeB].emplace_back(nodeA, i);
+            int u = edges[i][0], v = edges[i][1];
+            graph[u].emplace_back(v, i);
+            graph[v].emplace_back(u, i);
         }
 
-        vector<vector<int>> distances(n, vector<int>(2, INT_MAX));
-        distances[source][0] = distances[source][1] = 0;
+        vector<vector<int>> distance(n, vector<int>(2, INT_MAX));
+        distance[src][0] = distance[src][1] = 0;
 
-        runDijkstra(adjacencyList, edges, distances, source, 0, 0);
-        int difference = target - distances[destination][0];
-        if (difference < 0) return {}; 
-        runDijkstra(adjacencyList, edges, distances, source, difference, 1);
-        if (distances[destination][1] < target) return {}; 
+        dijkstra(graph, edges, distance, src, 0, 0);
+        int remaining = target - distance[dest][0];
+        if (remaining < 0) return {};
+
+        dijkstra(graph, edges, distance, src, remaining, 1);
+        if (distance[dest][1] < target) return {};
 
         for (auto& edge : edges) {
             if (edge[2] == -1) edge[2] = 1;
@@ -24,42 +25,31 @@ public:
     }
 
 private:
-    void runDijkstra(const vector<vector<pair<int, int>>>& adjacencyList, vector<vector<int>>& edges, vector<vector<int>>& distances, int source, int difference, int run) {
-        int n = adjacencyList.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> priorityQueue;
-        priorityQueue.push({0, source});
-        distances[source][run] = 0;
+    void dijkstra(const vector<vector<pair<int, int>>>& graph, vector<vector<int>>& edges, vector<vector<int>>& distance, int src, int adjust, int run) {
+        int n = graph.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, src});
+        distance[src][run] = 0;
 
-        while (!priorityQueue.empty()) {
-            auto [currentDistance, currentNode] = priorityQueue.top();
-            priorityQueue.pop();
+        while (!pq.empty()) {
+            auto [currDist, node] = pq.top();
+            pq.pop();
+            if (currDist > distance[node][run]) continue;
 
-            if (currentDistance > distances[currentNode][run]) continue;
+            for (auto& [neighbor, idx] : graph[node]) {
+                int weight = edges[idx][2];
+                if (weight == -1) weight = 1;
 
-            for (auto& neighbor : adjacencyList[currentNode]) {
-                int nextNode = neighbor.first, edgeIndex = neighbor.second;
-                int weight = edges[edgeIndex][2];
-
-                if (weight == -1) weight = 1; 
-
-                if (run == 1 && edges[edgeIndex][2] == -1) {
-                    int newWeight = difference + distances[nextNode][0] - distances[currentNode][1];
-                    if (newWeight > weight) {
-                        edges[edgeIndex][2] = weight = newWeight;
-                    }
+                if (run == 1 && edges[idx][2] == -1) {
+                    int newWeight = adjust + distance[neighbor][0] - distance[node][1];
+                    if (newWeight > weight) edges[idx][2] = weight = newWeight;
                 }
 
-                if (distances[nextNode][run] > distances[currentNode][run] + weight) {
-                    distances[nextNode][run] = distances[currentNode][run] + weight;
-                    priorityQueue.push({distances[nextNode][run], nextNode});
+                if (distance[neighbor][run] > distance[node][run] + weight) {
+                    distance[neighbor][run] = distance[node][run] + weight;
+                    pq.push({distance[neighbor][run], neighbor});
                 }
             }
         }
     }
 };
-static const auto mynameisbarryallen = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
