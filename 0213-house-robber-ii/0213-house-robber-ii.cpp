9a4fa954a1def574f0af093dc3c6bd1103a2cadc
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2025-01-18 11:39:48 +0530

    Time: 0 ms (100%), Space: 10.8 MB (37.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        vector<int>empty1=nums;
-        empty1.pop_back();
-        vector<int>empty2=nums;
-        vector<int>dp(nums.size(),-1);
+        
+        vector<int>frontMiss=nums;
         vector<int>dp1(nums.size(),-1);
-
-        reverse(empty2.begin(),empty2.end());
-        empty2.pop_back();
-
-        return max(solve(0,empty1,dp),solve(0,empty2,dp1));;
+        vector<int>dp2(nums.size(),-1);
+        reverse(frontMiss.begin(),frontMiss.end());
+        frontMiss.pop_back();
+        vector<int>BackMiss=nums;
+        BackMiss.pop_back();
+        return max(solve(0,frontMiss,dp1),solve(0,BackMiss,dp2));
     }
     int solve(int i,vector<int>&nums,vector<int>&dp){
         if(i>=nums.size())return 0;
@@ -19,8 +18,5 @@ public:
         int include=solve(i+2,nums,dp)+nums[i];
         int exclude=solve(i+1,nums,dp);
         return dp[i]=max(include,exclude);
-        
-            
-        }
-    
+    }
 };
