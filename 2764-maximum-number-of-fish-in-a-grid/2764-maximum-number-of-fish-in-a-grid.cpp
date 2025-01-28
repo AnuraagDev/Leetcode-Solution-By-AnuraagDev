7bbd0f3662476c2b836cd3b8cb65157a17de39b7
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2025-01-28 11:44:29 +0530

    Time: 0 ms (100%), Space: 93.9 MB (98.42%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -5,21 +5,20 @@ public:
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]!=0){
-                    vector<vector<int>>visited(grid.size(),vector<int>(grid[0].size(),0));
-                    ans=max(ans,solve(i,j,grid,visited));
+                 
+                    ans=max(ans,solve(i,j,grid));
                 }
             }
         }
         return ans;
     }
-    int solve(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&visited){
-        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==0||visited[i][j])return 0;
+    int solve(int i,int j,vector<vector<int>>&grid){
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==0)return 0;
         int dir[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
         int ans=grid[i][j];
-       
-        visited[i][j]=1;
+       grid[i][j]=0;
         for(int ind=0;ind<4;ind++){
-            ans+=solve(i+dir[ind][0],j+dir[ind][1],grid,visited);
+            ans+=solve(i+dir[ind][0],j+dir[ind][1],grid);
         }
         return ans;
     }
