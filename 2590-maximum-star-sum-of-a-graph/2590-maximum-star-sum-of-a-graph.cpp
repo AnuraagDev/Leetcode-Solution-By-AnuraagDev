188307705aff7773bf37e2b6353da20b1feb65eb
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2025-03-26 12:45:25 +0530

    Time: 85 ms (64.36%), Space: 136.4 MB (61.34%) - LeetHub

--- 2590-maximum-star-sum-of-a-graph/2590-maximum-star-sum-of-a-graph.cpp
+++ 2590-maximum-star-sum-of-a-graph/2590-maximum-star-sum-of-a-graph.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
          vector<vector<int>>adj(vals.size());
-         for(auto i:edges){
+         for(auto &i:edges){
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
          }
          int ans=INT_MIN;
          for(int i=0;i<vals.size();i++){
             vector<int>a;
-            for(auto i:adj[i]){
+            for(auto &i:adj[i]){
                 a.push_back(vals[i]);
             }
             sort(a.begin(),a.end(),greater<int>());
